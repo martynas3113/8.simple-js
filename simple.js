b1caf0759fab01a79f07d74bea1e821c80a93614
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-11 23:38:30 +0200

    final commit

---
+++ simple.js
@@ -0,0 +1,33 @@
+const one = 1;
+console.log(one)
+const two =2;
+console.log(two)
+const three = 3;
+console.log(three)
+
+// String
+
+const you = 'you'
+console.log(you)
+const me ='me';
+console.log(me)
+const he = 'he';
+console.log(he)
+
+// Arrays
+
+const array1 = [1,3,4,5,6]
+console.log(array1)
+const array2 = [3,4,5,6,7,7]
+console.log(array2)
+const array3 = [56,34,23,56,78]
+console.log(array3)
+
+// Arrays String
+
+const array4 = ['labas','rytas','tau']
+console.log(array4)
+const array5 = ['sveiki', 'atvyke' , 'cia']
+console.log(array5)
+const array6 = ['ahoy', 'captain' , 'yarrr']
+console.log(array6)
