9547d2d55c1599fdd58a54b16a733c6240c7c37d
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-11 23:39:12 +0200

    final-commit

--- simple.js
+++ simple.js
@@ -16,18 +16,45 @@ console.log(he)
 
 // Arrays
 
-const array1 = [1,3,4,5,6]
-console.log(array1)
-const array2 = [3,4,5,6,7,7]
-console.log(array2)
-const array3 = [56,34,23,56,78]
-console.log(array3)
+const array1 = [1,3,4,5,6];
+console.log(array1);
+const array2 = [3,4,5,6,7,7];
+console.log(array2);
+const array3 = [56,34,23,56,78];
+console.log(array3);
 
 // Arrays String
 
-const array4 = ['labas','rytas','tau']
-console.log(array4)
-const array5 = ['sveiki', 'atvyke' , 'cia']
-console.log(array5)
-const array6 = ['ahoy', 'captain' , 'yarrr']
-console.log(array6)
+const array4 = ['labas','rytas','tau'];
+console.log(array4);
+const array5 = ['sveiki', 'atvyke' , 'cia'];
+console.log(array5);
+const array6 = ['ahoy', 'captain' , 'yarrr'];
+console.log(array6);
+
+
+// Math
+
+
+const sum  = one + two + three;
+console.log(sum);
+const str = `${you} ${me} ${he}`;
+console.log(str);
+const sumArray = array1[0] - array1[1] + array1[2] - array1[3] + array1[4];
+console.log(sumArray);
+
+
+// 2 budas
+
+let sumaArray = array1[0]
+sumaArray = sumaArray - array1[1];
+sumaArray = sumaArray + array1[2];
+sumaArray = sumaArray - array1[3];
+sumaArray = sumaArray + array1[4];
+console.log(sumArray)
+
+
+// array str sum
+
+const strSum = array6.concat(array5,array4)
+console.log(strSum);
