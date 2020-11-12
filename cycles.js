63285d5915454fc992281626e0f6be6a60ffc20e
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-12 22:06:53 +0200

    final-practice

--- cycles.js
+++ cycles.js
@@ -72,21 +72,83 @@ console.clear();
 
 // Duodamas zodis ir is jo reikia isrinkti kas antra raide ir isspauzdinti kas gaunasi
 
-let text = 'abcdef';
-let result = '';
-let raide = '';
-const step = 2;
+// let text = 'abcdef';
+// let result = '';
+// let raide = '';
+// const step = 2;
 
-for (let i = step - 1;  i< text.length; i = i+step) {
-    raide = text[i];
-    result = result + raide;
+// for (let i = step - 1;  i< text.length; i = i+step) {
+//     raide = text[i];
+//     result = result + raide;
+    
+// }
+
+// console.log(result);
+
+// Apskaiciuoti skaiuciu suma intervale nuo [1:100] imtinai
+
+
+
+const pirmasRezis = -70;
+const antrasRezis = 30;
+let suma = 0;
+let rezultatas = 0;
+
+for (let i = pirmasRezis; i <= antrasRezis; i++) {
+    suma =  i;
+    rezultatas = rezultatas+suma;
+    
     
 }
+console.log(rezultatas);
 
+const daliklis = 3;
+const pirmasIntervalas = 0;
+const antrasintervalas = 11;
+let kiekis = 0;
+
+for (let i = pirmasIntervalas;  i<= antrasintervalas; i++) {
+    if (i % daliklis===0 ) {
+        kiekis = kiekis + 1
+    }
+}
+console.log(`Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš ${daliklis} yra ${kiekis} vienetai`);
+
+
+const pirmasIntervalas2 = 8;
+const antrasintervalas2 = 31;
+let kiekis2 = 0;
+
+for (let i = pirmasIntervalas2;  i<= antrasintervalas2; i++) {
+    if (i % 5===0 ) {
+        kiekis2 = kiekis2 + 1
+    }
+}
+console.log(`Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra ${kiekis2} vienetai`);
+
+
+const pirmasIntervalas3 = 0;
+const antrasintervalas3 = 11;
+let kiekis3 = 0;
+
+for (let i = pirmasIntervalas3;  i<= antrasintervalas3; i++) {
+    if (i % 7===0 ) {
+        kiekis3 = kiekis3 + 1
+    }
+}
+console.log(`Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra ${kiekis3} vienetai`);
+
+
+
+const zodis = 'abcdef'
+let result = '';
+
+for (let i = zodis.length - 1; i >=0 ; i--) {
+     result = result +zodis[i];
+    
+    
+}
 console.log(result);
-
-
-
 
 
 
